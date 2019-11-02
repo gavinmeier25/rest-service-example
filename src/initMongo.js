26eb1418984721b20ad63aca733c55e7020cc375
gavinmeier25 <gavinmeier41910@gmail.com>
2019-11-01 19:06:51 -0500

    ok

--- src/initMongo.js
+++ src/initMongo.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 
 const initMongo = () => {
-  mongoose.connect(
-    'mongodb://test:test9033@ds261077.mlab.com:61077/test-dashboard',
-    { useNewUrlParser: true, autoIndex: true, autoReconnect: true }
-  )
+  mongoose.connect(process.env.MLAB, {
+    useNewUrlParser: true,
+    autoIndex: true,
+    autoReconnect: true,
+  })
   mongoose.connection.once('open', () => {
     console.log('connected to mlab')
   })
